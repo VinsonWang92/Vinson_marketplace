6f2b72afbc6347df3b51f665174573c1fe98ef34
VinsonWang92 <vinsonwang92@gmail.com>
2020-11-17 11:10:35 +1100

    stripe api key changes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,9 @@
-Rails.configuration.stripe = {
-    :publishable_key => Rails.application.credentials.dig(:stripe, :public_key),
-    :secret_key => Rails.application.credentials.dig(:stripe, :private_key)
-  }
+# Rails.configuration.stripe = {
+#     :publishable_key => Rails.application.credentials.dig(:stripe, :public_key),
+#     :secret_key => Rails.application.credentials.dig(:stripe, :private_key)
+#   }
+Rails.configuration.stripe = { 
+  :publishable_key => 'pk_test_thekey', 
+  :secret_key => 'sk_test_thekey'
+}
   Stripe.api_key = Rails.application.credentials.dig(:stripe, :private_key)
