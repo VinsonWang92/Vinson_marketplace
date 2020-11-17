b1748e68778ba01457169ea517bb1d7e8e3ca4ad
VinsonWang92 <vinsonwang92@gmail.com>
2020-11-17 23:19:08 +1100

    updated stripe client ID

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
     end
 
     def stripe_url
-        "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe, :connect_client_id)}&scope=read_write"
+        "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe, :connect_client_id)}&scope=read_write&redirect_uri"
     end
 
     def stripe_connect_button # add this method
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+    # authenticate :user, lambda { |u| u.admin? } do
+    #   mount Sidekiq::Web => '/sidekiq'
+    # end
     get "/payments/success", to: "payments#success"
     resources :courses do
       resources :comments, module: :courses
     end
 
-  scope '/checkout' do
-    post 'create', to: 'checkout#create', as: 'checkout_create'
-    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
-    get 'success', to: 'checkout#success', as: 'checkout_success'
-  end
+  # scope '/checkout' do
+  #   post 'create', to: 'checkout#create', as: 'checkout_create'
+  #   get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+  #   get 'success', to: 'checkout#success', as: 'checkout_success'
+  # end
 
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
   root to: 'courses#index'
